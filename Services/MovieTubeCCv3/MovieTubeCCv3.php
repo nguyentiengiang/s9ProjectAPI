adfa08073d94c320bb47117854c3c744f215d527
Giang Nguyen Tien <ongteu@outlook.com>
2014-10-03 16:46:13 +0700

    Fix MovieTubeCCv3: - Complete English movie functions

--- Services/MovieTubeCCv3/MovieTubeCCv3.php
+++ Services/MovieTubeCCv3/MovieTubeCCv3.php
@@ -29,10 +29,12 @@ class MovieTubeCCv3 {
 
     public function enable() {
         $this->app->get('/', array($this, 'index'));
-//        $this->app->get('/GetCountry', array($this, 'getCountries'));
+        // for all movies
         $this->app->get('/GetCategory', array($this, 'getCategories'));
         $this->app->get('/AllFilms', array($this, 'getAllFilms'));
-//        $this->app->post('/postAdd', array($this, 'postAddNew'));
+        // for english movies
+        $this->app->get('/english/GetCategory', array($this, 'getCategoriesEng'));
+        $this->app->get('/english/AllFilms', array($this, 'getAllFilmsEng'));
         $this->app->run();
     }
 
@@ -45,57 +47,30 @@ class MovieTubeCCv3 {
     public function index() {
         
     }
-
-    public function getCountries() {
-        $result = null;
-        try {
-//            $cache = new NotORM_Cache_File("notorm.cache");
-            $db = $this->dbConnect();
-            $countries = $db->country()->select("id, name")->where("isHide = ?", 0);
-            if (count($countries)) {
-                $result = array();
-                foreach ($countries as $country) {
-                    $data = iterator_to_array($country);
-                    array_push($result, $data);
-                }
-            } else {
-                $this->app->response()->status(404);
-                $result = array('message' => 'Get Country List Fail!');
-            }
-        } catch (ResourceNotFoundException $e) {
-            $this->app->response()->status(404);
-            $result = array('message' => 'Resource Not Found!');
-        } catch (Exception $e) {
-            $this->app->response()->status(400);
-            $this->app->response()->header('X-Status-Reason', $e->getMessage());
-        }
-        $this->app->response()->header('X-Powered-By', 'ongteu');
-        $mediaType = $this->app->request()->getMediaType();
-        if ($mediaType == 'application/xml') {
-            $this->app->response()->header('Content-Type', 'application/xml');
-            echo \s9ProjectHelper\ArrayToXML::toXml($result, 'app');
-        } else {
-            $this->app->response->headers->set('Content-Type', 'application/json');
-            echo json_encode($result, JSON_NUMERIC_CHECK | JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
-        }
-    }
+    
+    /*
+     * For All Movies
+     */
 
     public function getCategories() {
         $result = null;
         try {
 //            $cache = new NotORM_Cache_File("notorm.cache");
             $db = $this->dbConnect();
-            $categories = $db->genre()->select("id, name")->where("isHide = ?", 0);
-            if (count($categories)) {
-                $result = array();
-                foreach ($categories as $category) {
-                    $data = iterator_to_array($category);
-                    array_push($result, $data);
-                }
-            } else {
-                $this->app->response()->status(404);
-                $result = array('message' => 'Get genre list Fail!');
+            $genres = $db->genre()->select("id as cateId, name as cateName")->where("isHide = ?", 0);
+            $countries = $db->country()->select("id as countryId, name as countryName")->where("isHide = ?", 0);
+            $result = array();
+            $arrCountries = array();
+            $arrGenres = array();
+            foreach ($countries as $country) {
+                $data = iterator_to_array($country);
+                array_push($arrCountries, $data);
             }
+            foreach ($genres as $genre) {
+                $data = iterator_to_array($genre);
+                array_push($arrGenres, $data);
+            }
+            $result = array("listCategory" => $arrGenres) + array("listCountry" => $arrCountries);
         } catch (ResourceNotFoundException $e) {
             $this->app->response()->status(404);
             $result = array('message' => 'Resource Not Found!');
@@ -113,24 +88,39 @@ class MovieTubeCCv3 {
             echo json_encode($result, JSON_NUMERIC_CHECK | JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
         }
     }
-    
+
     public function getAllFilms() {
         $result = null;
         try {
 //            $cache = new NotORM_Cache_File("notorm.cache");
+            $date = intval($this->app->request()->get('date'));
             $db = $this->dbConnect();
-            $cfs = $db->admod_config()->select("packageName, packageNameMarketing, developer, " .
-                    "status as admobEnable, strSeparatorYT, strSeparatorGD, versionApp, isParserOnline, youtube_api_key, " .
-                    "isHd, is_download");
-            if (count($cfs)) {
-                $result = array();
-                foreach ($cfs as $cf) {
-                    $data = iterator_to_array($cf);
-                    array_push($result, $data);
+            $films = $db->film("isHide = ? ", 0)->select("id, name, year , poster, directors, stars, runtime, releaseDate, plot, 
+                imdbRate, MPAARate, quanlity, movieLink, isHide, countryId, siteId")->where("updateDay BETWEEN FROM_UNIXTIME(?) AND NOW()", $date)->limit(3, 5);
+            if (count($films)) {
+                $arrFilms = array();
+                foreach ($films as $film) {
+//                    $data = iterator_to_array($film);
+                    $filmGenre = $db->film_genre("film_id = ?", $film['id'])->select("genre_id");
+                    $arrFG = array();
+                    foreach ($filmGenre as $fg) {
+                        array_push($arrFG, $fg['genre_id']);
+                    }
+                    $filmDetails = array("film" => array(
+                            "filmId" => $film['id'],
+                            "name" => $film['name'],
+                            "year" => $film['year'],
+                            "youtubeId" => $film['movieLink'],
+                            "thumb" => $film['poster'],
+                            "imdb" => $film['imdbRate'],
+                            "category" => $arrFG
+                    ));
+                    array_push($arrFilms, $filmDetails);
                 }
+                $result = array("now" => time()) + array("listFilm" => $arrFilms);
             } else {
                 $this->app->response()->status(404);
-                $result = array('message' => 'Get Package Setting Fail!');
+                $result = array('message' => 'Get Films Fail!');
             }
         } catch (ResourceNotFoundException $e) {
             $this->app->response()->status(404);
@@ -150,25 +140,22 @@ class MovieTubeCCv3 {
         }
     }
 
-    public function getConfigByID($id) {
+    /*
+     * For English Movies Only
+     */
+    
+    public function getCategoriesEng() {
         $result = null;
         try {
-            if (intval($id) !== 0) {
-                $this->db = $this->dbConnect();
-                $cf = $this->db->admod_config("id = ?", intval($id))->select(
-                        "app, packageName, packageNameMarketing, developer, admod_large, admod_small, 
-                        adspaceid, publisherid, `status`, is_download, strSeparatorYT, strSeparatorGD, 
-                        versionApp, isParserOnline, youtube_api_key, isHd");
-                if (!empty($cf[0])) {
-                    $result = $cf[0];
-                } else {
-                    $this->app->response()->status(404);
-                    $result = array('message' => 'Get Package Setting Fail!');
-                }
-            } else {
-                $this->app->response()->status(404);
-                $result = array('message' => 'Invalid Parameters!');
+//            $cache = new NotORM_Cache_File("notorm.cache");
+            $db = $this->dbConnect();
+            $genres = $db->genre()->select("id as cateId, name as cateName")->where("isHide = ?", 0);
+            $arrGenres = array();
+            foreach ($genres as $genre) {
+                $data = iterator_to_array($genre);
+                array_push($arrGenres, $data);
             }
+            $result = array("listCategory" => $arrGenres);
         } catch (ResourceNotFoundException $e) {
             $this->app->response()->status(404);
             $result = array('message' => 'Resource Not Found!');
@@ -187,47 +174,57 @@ class MovieTubeCCv3 {
         }
     }
 
-    public function postAddNew() {
-        $post = $this->app->request()->post();
+    public function getAllFilmsEng() {
         $result = null;
-        $vaildate = self::ValidateAppList($post);
-        if (empty($vaildate)) {
-            try {
-                $db = $this->dbConnect();
-                $db->transaction = "BEGIN";
-                $lastID = $db->admod_config()->insert(array(
-                    "app" => $post['appName'], "packageName" => $post['packageName'], "packageNameMarketing" => $post['packageNameMarketing'],
-                    "developer" => $post['developer'], "admod_large" => $post['admodLarge'], "admod_small" => $post['admodSmall'],
-                    "adspaceid" => $post['adspaceId'], "publisherid" => $post['publisherId'],
-                    "strSeparatorYT" => $post['strSeparatorYT'], "strSeparatorGD" => $post['strSeparatorGD'],
-                    "versionApp" => $post['versionApp'], "status" => intval($post['status']), "is_download" => $post['isDownload'],
-                    "isParserOnline" => intval($post['isParserOnline']), "youtube_api_key" => $post['youTubeApiKey'],
-                    "isHd" => intval($post['isHD'])
-                        )
-                );
-                $db->transaction = "COMMIT";
-                $items = $db->admod_config("id = ?", $lastID);
-                if (count($items) > 0) {
-                    $this->app->response()->status(200);
-                    $result = array('message' => 'Inserted');
-                } else {
-                    $this->app->response()->status(200);
-                    $result = array('message' => 'Inserted But Unknown');
+        try {
+//            $cache = new NotORM_Cache_File("notorm.cache");
+            $date = intval($this->app->request()->get('date'));
+            $db = $this->dbConnect();
+            $films = $db->film("isHide = ? ", 0)->select("id, name, year , poster, directors, stars, runtime, releaseDate, plot, 
+                imdbRate, MPAARate, quanlity, movieLink, isHide, countryId, siteId")
+                    ->where("updateDay BETWEEN FROM_UNIXTIME(?) AND NOW()", $date);
+            if (count($films)) {
+                $arrFilms = array();
+                foreach ($films as $film) {
+                    $filmGenre = $db->film_genre("film_id = ?", $film['id'])
+                            ->select("genre_id")
+                            ->where("genre_id < 23");
+                    $arrFG = array();
+                    foreach ($filmGenre as $fg) {
+                        array_push($arrFG, $fg['genre_id']);
+                    }
+                    $filmDetails = array("film" => array(
+                            "filmId" => $film['id'],
+                            "name" => $film['name'],
+                            "year" => $film['year'],
+                            "youtubeId" => $film['movieLink'],
+                            "thumb" => $film['poster'],
+                            "imdb" => $film['imdbRate']
+                        )) + array("category" => $arrFG);
+                    array_push($arrFilms, $filmDetails);
                 }
-            } catch (\PDOException $e) {
-                $db->transaction = "ROLLBACK";
-                $result = array('message' => 'Validate broken!', 'errors' => $e->getMessage());
-            } catch (ResourceNotFoundException $e) {
+                $arrDel = array();
+                if ($date != 0) {
+                    $delIds = $db->film(array("countryId = ?" => 1, "isHide = ?" => 1))
+                            ->select("id")
+                            ->where("updateDay BETWEEN FROM_UNIXTIME(?) AND NOW()", $date);
+                    $arrDelTemp = array();
+                    foreach ($delIds as $delId) {
+                        array_push($arrDelTemp, $delId['id']);
+                    }
+                    $arrDel = array("deletedId" => $arrDelTemp);
+                }
+                $result = array("now" => time()) + array("listFilm" => $arrFilms) + $arrDel;
+            } else {
                 $this->app->response()->status(404);
-                $result = array('message' => 'Resource Not Found!', 'errors' => $e->getMessage());
-            } catch (Exception $e) {
-                $this->app->response()->status(400);
-                $this->app->response()->header('X-Status-Reason', $e->getMessage());
-                $result = array('message' => 'Unknown', 'errors' => $e->getMessage());
+                $result = array('message' => 'Get English Films Fail!');
             }
-        } else {
-            $this->app->response()->status(200);
-            $result = array('status' => 'error', 'errors' => $vaildate);
+        } catch (ResourceNotFoundException $e) {
+            $this->app->response()->status(404);
+            $result = array('message' => 'Resource Not Found!');
+        } catch (Exception $e) {
+            $this->app->response()->status(400);
+            $this->app->response()->header('X-Status-Reason', $e->getMessage());
         }
         $this->app->response()->header('X-Powered-By', 'ongteu');
         $mediaType = $this->app->request()->getMediaType();
@@ -238,70 +235,6 @@ class MovieTubeCCv3 {
             $this->app->response->headers->set('Content-Type', 'application/json');
             echo json_encode($result, JSON_NUMERIC_CHECK | JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
         }
-    }
-
-    public function postUpdateByID($id) {
-        $request = $this->app->request();
-        $name = $request->post('name');
-        $url = $request->post('url');
-        try {
-            $sql = "INSERT INTO commodores (name, url) VALUES (:name, :url)";
-            $s = $this->db->prepare($sql);
-            $s->bindParam("name", $name);
-            $s->bindParam("url", $url);
-            $s->execute();
-        } catch (\PDOException $e) {
-            echo 'Exception: ' . $e->getMessage();
-        }
-    }
-
-    public function putItem($id) {
-        $request = $this->app->request();
-        $name = $request->put('name');
-        $url = $request->put('url');
-
-        try {
-            $sql = "update commodores set url=:url, name=:name where id=:id";
-            $s = $this->db->prepare($sql);
-            $s->bindParam("id", $id);
-            $s->bindParam("name", $name);
-            $s->bindParam("url", $url);
-            $s->execute();
-        } catch (\PDOException $e) {
-            echo 'Exception: ' . $e->getMessage();
-        }
-    }
-
-    public function deleteItem($id) {
-        try {
-            $sql = "delete from commodores where id=:id";
-            $s = $this->db->prepare($sql);
-            $s->bindParam("id", $id);
-            $s->execute();
-        } catch (\PDOException $e) {
-            echo 'Exception: ' . $e->getMessage();
-        }
-    }
-
-    private function ValidateAppList($arrayData) {
-        $return = null;
-        Valitron\Validator::langDir(LIB_ROOT . 'Valitron/lang');
-        Valitron\Validator::lang('vi');
-        $v = new Valitron\Validator($arrayData);
-        Valitron\Validator::addRule('myBool', function($field, $value, array $params) {
-                    $acceptable = array('0', '1', 0, 1, false, true);
-                    return in_array($value, $acceptable, true);
-                }, 'không đúng định dạng');
-        $v->rule('required', ['appName', 'packageName', 'packageNameMarketing', 'developer', 'admodLarge', 'admodSmall', 'adspaceId', 'publisherId', 'strSeparatorYT', 'strSeparatorGD', 'versionApp', 'status', 'isDownload', 'isParserOnline', 'youTubeApiKey', 'isHD']);
-        $v->rule('myBool', ['isDownload', 'isParserOnline', 'isHD']);
-        $v->rule('integer', ['id']);
-        $v->rule('min', ['id'], 1);
-        if ($v->validate()) {
-            $return = null;
-        } else {
-            $return = $v->errors();
-        }
-        return $return;
     }
 
 }
